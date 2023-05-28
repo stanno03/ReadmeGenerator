026734a90f4a806711b53e4b056ee707072ea8e0
stanno03 <danno_41@hotmail.com>
2023-05-28 13:47:38 +1000

    fixed switch statement as it was returning undefined

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,50 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-      
+
+  let licenseBadge = "";
   switch(license){
 
-    case license === "MIT": {
-      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    case "MIT": {
+      return licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+     
       
     }
-    case license === "MPL": {
-      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    case "Mozilla": {
+      return licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
     }
 
-    case license === "Apache 2.0": {
-      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    case "Apache 2.0": {
+      return licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+     
     }
 
-    case license === "IBM": {
-      return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`
-    }
-    default: return " ";
+    case "IBM": {
+      return licenseBadge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
+     
   }
-
+  default: return licenseBadge;
   }
+}
+ 
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  let licenseLink = "";
   switch(license){
 
-    case license === "MIT": {
-      return `[![License: MIT](https://opensource.org/licenses/MIT)`
+    case "MIT": {
+      return licenseLink = '[Link to the MIT Licence can be found here](https://opensource.org/licenses/MIT)';
+     
       
     }
-    case license === "Mozilla": {
-      return `[![License: MPL 2.0](https://opensource.org/licenses/MPL-2.0)`
+    case "Mozilla": {
+      return licenseLink ='[![Link to the MPL Licence can be found here(https://opensource.org/licenses/MPL-2.0)';
+     
     }
 
-    case license === "Apache 2.0": {
-      return `[![License](https://opensource.org/licenses/Apache-2.0)`
+    case "Apache 2.0": {
+      return licenseLink ='[![Link to the Apache 2.0 Licence can be found here](https://opensource.org/licenses/Apache-2.0)';
+     
     }
 
-    case license === "IBM": {
-      return `[![License: IPL 1.0](https://opensource.org/licenses/IPL-1.0)`
-    }
-    default: return " ";
+    case "IBM": {
+      return licenseLink = '[![Link to the IPL Licence can be found here](https://opensource.org/licenses/IPL-1.0)';
+     
+  }
+  default: return licenseLink;
   }
 }
 
@@ -52,38 +62,50 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(license){
-    return `## licence`;
+
+    return `${renderLicenseBadge(license)}
+    ${renderLicenseLink(license)}`
+     
   }else " ";
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
-  return `
-
-  ## ${answers.title}
  
-  ## Table of Contents
-  *[Description](#description)
-
+  return `
+  
+  ## ${answers.title}
+  ${renderLicenseBadge(license)}
+ 
   ## Description
+
   # ${answers.description}
 
+  ## Table of Contents
+
+  [Description](#description) 
+  
+
+  
+
   ## Installation
+
   # ${answers.installation}
 
   ## Usage
+
   # ${answers.usage}
 
   ## Credits
+
   # ${answers.credits}
 
-  ## Licence
-  # ${answers.license}
-  ${renderLicenseBadge()}
-  ${renderLicenseLink()}
+  ## Licence: 
+  ${renderLicenseSection(answers.license)}
+
+  
 
 
 `;
 }
-
 module.exports = generateMarkdown;
